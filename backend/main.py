cb04a9d2e3c43054389c9ac6233a917fa9078804
ProArt1st <ProArt1st@outlook.com>
2026-09-20 18:20:22 +0800

    抽取配置文件，补充.env.example

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,12 @@ from pypinyin import lazy_pinyin, Style
 from snownlp import SnowNLP
 from storage import init_db, save_record, get_history
 from datetime import datetime, timezone
+import os
+from dotenv import load_dotenv
+
+load_dotenv()                        # ← 读同目录下的 .env
+
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS").split(",")
 
 init_db()
 
@@ -13,10 +19,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=ALLOWED_ORIGINS,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
-    allow_credentials=True, 
+    allow_credentials=True,
 )
 
 def get_session_id(request: Request, response: Response) -> str:
